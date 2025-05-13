37543e0199d0f8a292e43aa29a1ac35ce592429e
CG-Tespy <gbcabreracgt@gmail.com>
2025-05-13 19:19:28 -0400

    A little bit of the foundation

---
+++ Assets/Fungus/Scripts/NewSystems/NewSaveSys/Core/ISaveData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Amanita.SaveSys
+{
+    public interface ISaveData
+    {
+        
+    }
+}
---
+++ Assets/Fungus/Scripts/NewSystems/NewSaveSys/Events/SaveSysEvents.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Amanita.SaveSys
+{
+    public static class SaveSysEvents 
+    {
+        
+    }
+}
