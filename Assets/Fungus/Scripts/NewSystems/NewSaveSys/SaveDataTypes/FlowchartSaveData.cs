59e048a69d4f2796e00ba46fe83bca300cc8bbe9
CG-Tespy <gbcabreracgt@gmail.com>
2025-05-18 12:51:22 -0400

    Adding in tests for saving Blocks and Flowcharts
    FlowchartSaveData saves the flowchart's unique id instead of the id of a SaveIdentifier (+3 squashed commit)
    
    Squashed commit:
    
    [877ab83e] Added some tests for FlowchartSaveData
    
    [e3cabfa8] Add test for Block ID | Removed redundant class
    
    [44f5fc1d] Reorganized scripts | Added tests for BlockSaveData | BlockSaveData serializes properly now

--- Assets/Fungus/Scripts/NewSystems/NewSaveSys/SaveDataTypes/FlowchartSaveData.cs
+++ Assets/Fungus/Scripts/NewSystems/NewSaveSys/SaveDataTypes/FlowchartSaveData.cs
@@ -8,37 +8,69 @@ namespace Amanita.SaveSys
     [System.Serializable]
     public class FlowchartSaveData : SaveData
     {
-        // Note that no two vars in the same Flowchart can share the same name/key. That's why 
-        // we can have these two lists of keys and values.
-        // Also, when finding which flowchart this should be applied to, we search
+        // When finding which flowchart this should be applied to, we search
         // by ID first. If not found, then we search by name.
-        [SerializeField] protected string flowchartID = string.Empty;
+        [SerializeField] protected string uniqueID = string.Empty;
         [SerializeField] protected string flowchartName = string.Empty;
-        [SerializeField] protected List<string> varIDs = new();
-        [SerializeField] protected List<string> varNames = new(); 
-        // ^For when we can't find a var based on the ID
-        [SerializeField] protected List<string> values = new();
-        [SerializeField] protected List<string> variables = new();
+        [SerializeField] protected List<VariableSaveData> savedVars = new();
+        [SerializeField] protected List<BlockSaveData> savedBlocks = new();
+
+        public virtual string UniqueId
+        {
+            get => uniqueID;
+            set => uniqueID = value;
+        }
+
+        public virtual string FlowchartName
+        {
+            get => flowchartName;
+            set => flowchartName = value;
+        }
+
+        public virtual IList<VariableSaveData> SavedVars
+        {
+            get => savedVars;
+        }
 
         public FlowchartSaveData(Flowchart toCreateFrom)
         {
-            SaveIdentifier identifier = toCreateFrom.GetComponent<SaveIdentifier>();
-            flowchartID = identifier.UniqueID;
+            uniqueID = toCreateFrom.UniqueId;
             flowchartName = toCreateFrom.name;
+            SaveVars(toCreateFrom);
+            SaveBlocks(toCreateFrom);
 
+            // TODO: Save the state of certain commands (such as Conversation)
+
+        }
+
+        protected virtual void SaveVars(Flowchart toCreateFrom)
+        {
             foreach (Variable varEl in toCreateFrom.Variables)
             {
                 IVarEncoder forThisVar = EncoderRegistry.GetEncoder(varEl);
                 if (forThisVar == null)
                 {
-                    Debug.LogError($"No serializer found for variable type: {varEl.GetType().Name}");
+                    Debug.LogWarning($"No serializer found for variable type: {varEl.GetType().Name}");
                     continue;
                 }
 
-                varIDs.Add(varEl.UniqueId);
-                varNames.Add(varEl.Key);
-                string encodedData = forThisVar.EncodeToString(varEl);
-                values.Add(encodedData);
+                VariableSaveData varSave = forThisVar.Encode(varEl);
+                if (varSave == null)
+                {
+                    Debug.LogError($"Failed to encode variable: {varEl.name}");
+                    continue;
+                }
+
+                savedVars.Add(varSave);
+            }
+        }
+
+        protected virtual void SaveBlocks(Flowchart toCreateFrom)
+        {
+            foreach (Block block in toCreateFrom.GetExecutingBlocks())
+            {
+                BlockSaveData blockSave = new(block);
+                savedBlocks.Add(blockSave);
             }
         }
 
@@ -50,50 +82,6 @@ namespace Amanita.SaveSys
             return newItem;
         }
 
-        public static void ApplyFlowchartSaveData(Flowchart flowchart, FlowchartSaveData saveData)
-        {
-            Dictionary<string, object> variables = new();
-
-            for (int i = 0; i < saveData.varIDs.Count; i++)
-            {
-                string key = saveData.varIDs[i];
-                Variable toApplyTo = flowchart.GetVariable(key);
-
-                string value = saveData.values[i];
-
-
-            }
-
-        }
-
-        private static Transform FindTransformByPath(Transform root, string[] path)
-        {
-            foreach (string step in path)
-            {
-                root = root.Find(step);
-                if (root == null) return null;
-            }
-            return root;
-        }
     }
 
-    [System.Serializable]
-    public class VarSaveData<T> : SaveData
-    {
-        [SerializeField] protected string key = string.Empty;
-        [SerializeField] protected string uniqueID = string.Empty;
-        [SerializeField] protected string valueStr = string.Empty;
-
-        protected T value;
-        public virtual string Key => key;
-        public virtual string UniqueID => uniqueID;
-        public virtual string ValueStr => valueStr;
-        public virtual T Value => value;
-
-        public override SerializedSaveData Serialized()
-        {
-            SerializedSaveData newItem = new SerializedSaveData(GetType().Name, JsonUtility.ToJson(this, true));
-            throw new System.NotImplementedException();
-        }
-    }
 }
