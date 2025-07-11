99c964f98347855d9a14b2b3d4f156bc7d64844e
CG-Tespy <gbcabreracgt@gmail.com>
2025-07-11 05:57:29 -0400

    Basic implementation | Added tests | Using .NET Framework instead of .NET standard so we can use the dynamic keyword

---
+++ Assets/Tests/PlayMode/VarOverhaul/MuscariableTestsCommon.cs
@@ -0,0 +1,12 @@
+namespace Amanita.MuscariableTests
+{
+    public abstract class MuscariableTestsCommon
+    {
+        protected const int SampleInt = 10;
+        protected const float SampleF = 2.5f;
+        protected const double SampleD = 3.5;
+        protected const string SampleS = "hello";
+        protected const string OtherS = "world";
+
+    }
+}
