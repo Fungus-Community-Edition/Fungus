b201490307918c6c0a4eb3be36b862c17be78ad1
CG-Tespy <gbcabreracgt@gmail.com>
2026-07-21 14:29:43 -0400

    Asset removals
    - Hyphlow installation
    - Stuff that depended on Hyphlow (mostly tests)
    - Removed FungusLua (it shall likely end up in its own repo)

--- Assets/FungusLua/Thirdparty/MoonSharp/Interpreter/Execution/Scopes/IClosureBuilder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MoonSharp.Interpreter.Execution
-{
-	internal interface IClosureBuilder
-	{
-		SymbolRef CreateUpvalue(BuildTimeScope scope, SymbolRef symbol);
-
-	}
-}
--- Assets/Tests/PlayMode/Tweens/DefaultAdapterTests.cs
+++ Assets/Tests/PlayMode/Tweens/DefaultAdapterTests.cs
@@ -1,5 +1,4 @@
 using AtMycelia.Amanita;
-using AtMycelia.Hyphlow.Sys;
 using AtMycelia.HyphaTween;
 using NUnit.Framework;
 using UnityEngine;
